8c47f29b7d574e1eb8684235c22c4d63e9a7d6b4
varcessaro <66271718+varcessaro@users.noreply.github.com>
2020-07-11 12:38:28 -0400

    Working on implementing pixel cleanup

--- src/dpc_contrast.h
+++ src/dpc_contrast.h
@@ -6,6 +6,12 @@
 #include <math.h>
 #include "kmz_core.h"
 #include "kmz_color.h"
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
+#define DPC_THREADED 0
+#else
+#include <pthread.h>
+#define DPC_THREADED 1
+#endif
 
 void dpc_perform_contrast(KmzImage * img, size_t argc, KmzArgbColor * argv);
 
